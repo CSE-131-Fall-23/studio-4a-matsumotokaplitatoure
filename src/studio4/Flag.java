802f03b38b8d3a70dbb7f2e4626fc11fd65fa56e
lauren <lauren@10.232.165.155>
2023-10-04 15:46:38 -0500

    flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,16 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
-		
+	StdDraw.clear();
+	StdDraw.setXscale(0, 66);
+	StdDraw.setYscale(0, 48);
+	StdDraw.setPenColor(173,216,230);
+	StdDraw.filledRectangle(33, 8, 33, 8);
+	StdDraw.setPenColor(0,0,100);
+	StdDraw.filledRectangle(33, 32, 33, 16);
+	StdDraw.setPenColor(255,255,204);
+	StdDraw.filledEllipse(33,32,10,8);
 	}
+	
+		   
 }
